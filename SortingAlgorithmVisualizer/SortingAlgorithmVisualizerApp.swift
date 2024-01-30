79c395c5229a7f6857a0ea81c8a70b6995e4ff91
Neal Archival <73256760+nealarch01@users.noreply.github.com>
2024-01-30 10:21:06 -0800

    Initial Commit

---
+++ SortingAlgorithmVisualizer/SortingAlgorithmVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SortingAlgorithmVisualizerApp.swift
+//  SortingAlgorithmVisualizer
+//
+//  Created by Neal Archival on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SortingAlgorithmVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
